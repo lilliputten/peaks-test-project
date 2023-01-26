8ad7c77f6d2690e89bbf8070b3e3667537f2aba5
lilliputten <lilliputten@yandex.ru>
2023-01-26 18:21:04 +0700

    Build publish.0.0.0, 2023.01.26, 18:07 (230126-1807)

---
+++ _next/static/ESF7sugUQwvjInyLEtNKU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8c99747774c7239c.css","static/chunks/pages/index-f968e545e8b50449.js"],"/_error":["static/chunks/pages/_error-21c4048e16cf5944.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
