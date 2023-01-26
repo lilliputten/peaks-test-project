36e9db3b7e1a7a520fe28c6cb18b8d91d255edcc
lilliputten <lilliputten@yandex.ru>
2023-01-26 19:34:05 +0700

    Added project config.

---
+++ src/config/app.ts
@@ -0,0 +1,9 @@
+/** @module config.app
+ *  @since 2023.01.26, 18:28
+ *  @changed 2023.01.26, 18:28
+ */
+
+import * as build from './build';
+
+export const DEBUG = build.DEBUG;
+
---
+++ src/config/site.ts
@@ -0,0 +1,21 @@
+/** @module config.site
+ *  @since 2023.01.26, 18:28
+ *  @changed 2023.01.26, 18:28
+ */
+
+export const titleDelim = ' — ';
+export const siteUrl = 'https://http://peaks.lilliputten.ru//';
+export const title = 'Peaks';
+export const descr =
+  'Peaks demo site';
+export const keywords = [
+  'peaks',
+  'react',
+  'demo',
+];
+export const faviconUrl = '/static/favicon.ico';
+export const faviconPngUrl = '/static/favicon-120x120.png';
+export const faviconSvgUrl = '/static/favicon.svg';
+export const opImageUrl = '/static/logo-256.png';
+export const opImageWidth = 256;
+export const opImageHeight = 256;
